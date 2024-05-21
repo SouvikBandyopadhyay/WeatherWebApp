d89657d89503814ae2cca0fb013a3905e6f2f8d7
SouvikBandyopadhyay <80415021+SouvikBandyopadhyay@users.noreply.github.com>
2024-05-21 13:07:17 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const channelID = '2555630';
+const readAPIKey = 'B1XRRDRXCFNW2DL3'; // Optional if your channel is public
+
+async function fetchData() {
+    const url = `https://api.thingspeak.com/channels/${channelID}/feeds.json?results=1&api_key=${readAPIKey}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        const channelData = data.channel;
+        const feedsData0 = data.feeds[0];
+        for (let index = 1; index <= 4; index++) {
+            if (feedsData0[`field${index}`] !== null) {
+                document.getElementById(channelData[`field${index}`].replace(" ","_")).innerText = feedsData0[`field${index}`];
+            }
+            
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+setInterval(fetchData, 2000);
+fetchData();
